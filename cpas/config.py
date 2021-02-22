3ce0fe86c2527d623a69ca8996df2947a510073a
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2021-02-22 17:10:48 +0000

    add module to handle configuration

---
+++ cpas/config.py
@@ -0,0 +1,135 @@
+
+# This file is part of cpas.
+#
+# cpas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cpas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cpas.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2020 cpas team
+
+from configobj import ConfigObj, flatten_errors
+from validate import Validator
+from pathlib import Path
+
+# the defaults
+defaultCfgStr = """
+[inputs]
+# base path to input files, all inputs are relative to this directory
+inputbase = string
+# name of tiff file containing landcover
+landcover = string
+# name of shape file containing roads
+roads = string
+# name of tiff file containing DEM
+dem = string
+# walking speed
+landcover_ws = string
+roads_ws = string
+
+# factor applied to walking speed when walking with children
+child_impact = float(default=0.78)
+
+# whether to include small paths
+include_small_paths = boolean(default=True)
+
+
+# Water speed for water passable layer
+waterspeed = float(default=1.5)
+
+
+[outputs]
+# base path for output files
+outputbase = string
+costsurface = string
+costsurface_water = string
+"""
+
+cpasDefaults = ConfigObj(defaultCfgStr.split('\n'),list_values=False,_inspec=True)
+validator = Validator()
+
+class CpasConfig:
+    def __init__(self):
+        self._cfg = ConfigObj(configspec=cpasDefaults)
+        self._cfg.validate(validator)
+
+    def read(self,fname:Path):
+        """read and parse configuration file"""
+
+        fname = Path(fname)
+        
+        if not fname.is_file():
+            msg = f'no such configuration file {fname}'
+            raise RuntimeError(msg)
+
+        self._cfg.filename = str(fname)
+        self._cfg.reload()
+        if not self._cfg.validate(validator):
+            msg = f'Could not read config file {fname}'
+            raise RuntimeError(msg)
+
+    @property
+    def cfg(self):
+        return self._cfg
+
+    @property
+    def inputbase(self):
+        return Path(self.cfg['inputs']['inputbase'])
+    @property
+    def landcover(self):
+        return str(self.inputbase/Path(self.cfg['inputs']['landcover']))
+    @property
+    def roads(self):
+        return str(self.inputbase/Path(self.cfg['inputs']['roads']))
+    @property
+    def dem(self):
+        return str(self.inputbase/Path(self.cfg['inputs']['dem']))
+    @property
+    def landcover_ws(self):
+        return str(self.inputbase/Path(self.cfg['inputs']['landcover_ws']))
+    @property
+    def roads_ws(self):
+        return str(self.inputbase/Path(self.cfg['inputs']['roads_ws']))
+    @property
+    def child_impact(self):
+        return self.cfg['inputs']['child_impact']
+    @property
+    def include_small_paths(self):
+        return self.cfg['inputs']['include_small_paths']
+    @property
+    def waterspeed(self):
+        return self.cfg['inputs']['waterspeed']
+    @property
+    def outputbase(self):
+        return Path(self.cfg['outputs']['outputbase'])
+    @property
+    def costsurface(self):
+        return str(self.outputbase/Path(self.cfg['outputs']['costsurface']))
+    @property
+    def costsurface_water(self):
+        return str(self.outputbase/Path(self.cfg['outputs']['costsurface_water']))
+    
+   
+if __name__ == '__main__':
+    import sys
+    from pprint import pprint 
+
+    cfg = CpasConfig()
+
+    if len(sys.argv)>1:
+        cfg.read(Path(sys.argv[1]))
+
+    pprint(cfg.cfg.dict())
+
+    for c in ['landcover','roads','dem','landcover_ws','roads_ws',
+              'child_impact', 'include_small_paths','waterspeed',
+              'costsurface','costsurface_water']:
+        print (c,getattr(cfg,c))
--- cpas/creation.py
+++ cpas/creation.py
@@ -19,7 +19,6 @@
 creation.py: A python script to create a cost surface from land cover, road and DEM datasets.
 """
 
-import configparser
 import sys
 from pathlib import Path
 import resource
@@ -30,52 +29,21 @@ from . import tifs
 import numpy as np
 
 from .costsurface import lc, r, s, cs
+from .config import CpasConfig
 
 if __name__ == '__main__':
 
     start = timer()
 
     # read configuration file
-    cfg = configparser.ConfigParser()
+    cfg = CpasConfig()
     cfg.read(sys.argv[1])
-    
-    #Inputs and Outputs
-
-    #path to files
-    inputbase = Path(cfg['inputs']['inputbase'])
-
-    # Landcover
-    lc_inp = str(inputbase/cfg['inputs']['landcover'])
-    # Roads
-    r_inp = str(inputbase/cfg['inputs']['roads'])
-    # DEM
-    dem_inp = str(inputbase/cfg['inputs']['dem'])
-
-    # Walking Speeds
-    lc_ws = str(inputbase/cfg['inputs']['landcover_ws'])
-    r_ws = str(inputbase/cfg['inputs']['roads_ws'])
-
-    # Child Impact
-    child_impact = float(cfg['inputs']['child_impact'])
-
-    # Paths
-    paths = bool(cfg['inputs']['include_small_paths'])
-
-    # Water speed for water passable layer
-    waterspeed = float(cfg['inputs']['waterspeed'])
-
-    # Output
-    outputbase = Path(cfg['outputs']['outputbase'])
-    cs_out = str(outputbase/cfg['outputs']['costsurface'])
-    
-    cs_water_out = str(outputbase/cfg['outputs']['costsurface_water'])
-
 
     #Processes
 
     # Get tiff attributes from land cover file, these will be used for other processes
-    tiff_attributes = tifs.tiffHandle(lc_inp)
-    tiff_attributes.readTiff(lc_inp)
+    tiff_attributes = tifs.tiffHandle(cfg.landcover)
+    tiff_attributes.readTiff(cfg.landcover)
     # Create in-memory driver with same attributes as the landcover input
     # This will be used as a template for resampling and rasterizing
     tiff_attributes.emptyTiff()
@@ -84,31 +52,31 @@ if __name__ == '__main__':
     land_cover = tiff_attributes.data
 
     # Convert land cover to walking speeds
-    land_cover_speeds = lc.lc_to_ws(land_cover, lc_ws)
+    land_cover_speeds = lc.lc_to_ws(land_cover, cfg.landcover_ws)
 
     # Convert roads to walking speeds
-    roads = r.r_to_ws(r_inp, r_ws, paths, tiff_attributes)
+    roads = r.r_to_ws(cfg.roads, cfg.roads_ws, cfg.include_small_paths, tiff_attributes)
 
     # Combine roads and landcover walking speeds to make one surface
     ws_surface = r.backfill(land_cover_speeds, roads)
 
     # Create slope impact surface
-    slope_impact = s.dem_to_slope_impact(dem_inp, tiff_attributes,tempfile=str(outputbase/'resampleslope.tif'))
+    slope_impact = s.dem_to_slope_impact(cfg.dem, tiff_attributes,tempfile=str(cfg.outputbase/'resampleslope.tif'))
 
     # Include impact of slope in walking speeds surface
     ws_surface = ws_surface*(slope_impact/100)
 
     # Convert walking speed surface to a cost surface
-    cost_surface = cs.ws_to_cs(ws_surface, child_impact, tiff_attributes.res)
+    cost_surface = cs.ws_to_cs(ws_surface, cfg.child_impact, tiff_attributes.res)
 
     # Cost surface Output to tiff (water impassable)
     tiff_attributes.data = cost_surface
-    tiff_attributes.writeTiff(cs_out)
+    tiff_attributes.writeTiff(cfg.costsurface)
 
 
 
     # Add water as passable
-    land_cover = np.where(land_cover == 10, waterspeed, np.NaN)
+    land_cover = np.where(land_cover == 10, cfg.waterspeed, np.NaN)
 
     # convert water speed to time
     # 1 as children arnt slower than adults on motor boats... 
@@ -121,7 +89,7 @@ if __name__ == '__main__':
 
     # Output water cost surface to tiff
     tiff_attributes.data = cost_surface
-    tiff_attributes.writeTiff(cs_water_out)
+    tiff_attributes.writeTiff(cfg.costsurface_water)
 
 
     end = timer()
